378f7b291d1bb416e99022ce989db3fc1c6b990a
Jefferson Abreu <jehffersson@hotmail.com>
2024-09-15 18:31:34 -0300

    add O or X selected

--- main.py
+++ main.py
@@ -3,24 +3,56 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "DashDog"
     page.theme_mode = ft.ThemeMode.LIGHT
+    select_icon = None
 
-    def on_message(msg):
-        messages.controls.append(ft.Text(msg))
-        page.update()
-    
-    page.pubsub.subscribe(on_message)
+    def switch_player(icon: ft.Icon):
+        players.controls.append(ft.Row([icon, ft.Text(icon.tooltip)]))
+        players.update()
+  
+    page.pubsub.subscribe(switch_player)
 
-    def send_click(e):
-        page.pubsub.send_all(f"{user.value}: {message.value}")
-        message.value = ""
-        page.update()
+    def select_player_click(e):
+        if user.value != '':
+            icon : ft.Icon = ft.Icon(
+                name=e.control.icon,
+                color=e.control.icon_color,
+                size=e.control.icon_size,
+                tooltip = user.value
+            )            
+            page.pubsub.send_all(icon)
+            for icon in x_or_o.controls:
+                if e.control.icon == icon.icon:
+                    e.control.visible = False
+            page.update()
+        else:
+            page.open(
+                ft.AlertDialog(
+                    title=ft.Text("Erro"),
+                    content=ft.Text("Digite seu Nome"),
+                )
+            )
+        
+    players = ft.Column()
     
-    
-    messages = ft.Column()
+
     user = ft.TextField(hint_text="seu nome")
-    message = ft.TextField(hint_text="sua mensagem...", expand=True)
-    send = ft.ElevatedButton(text="Enviar", on_click=send_click)
-    page.add(messages, ft.Row([user, message, send]))
+
+    x_or_o = ft.Row(
+        controls=[
+            ft.IconButton(
+                icon=ft.icons.CIRCLE_OUTLINED,
+                icon_color=ft.colors.GREEN_ACCENT_400,
+                on_click=select_player_click
+            ),
+            ft.IconButton(
+                icon=ft.icons.CLOSE_OUTLINED,
+                icon_color=ft.colors.RED,
+                on_click=select_player_click
+            ),
+        ]
+    )
+    
+    page.add(players, ft.Row([user, x_or_o]))
 
 
 if __name__ == "__main__":
