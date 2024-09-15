806d62bf66e6d2bc0ac36964996eb27e0664061e
Jefferson Abreu <jehffersson@hotmail.com>
2024-09-15 15:12:52 -0300

    add __main__

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "DashDog"
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.add(ft.Row([ft.Icon(name=ft.icons.APPLE, size=35, color=ft.colors.BLACK)]))
 
-ft.app(main)    
+
+if __name__ == "__main__":
+    ft.app(main)
