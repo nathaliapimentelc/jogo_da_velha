b70f26d49260191c0e86343013723461758bb3c4
nathalia <nathaliapimentelc@hotmail.com>
2024-09-15 17:14:34 -0300

    add pubsub

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "DashDog"
     page.theme_mode = ft.ThemeMode.LIGHT
-    page.add(ft.Row([ft.Icon(name=ft.icons.APPLE, size=35, color=ft.colors.BLACK)]))
+
+    def on_message(msg):
+        messages.controls.append(ft.Text(msg))
+        page.update()
+    
+    page.pubsub.subscribe(on_message)
+
+    def send_click(e):
+        page.pubsub.send_all(f"{user.value}: {message.value}")
+        message.value = ""
+        page.update()
+    
+    
+    messages = ft.Column()
+    user = ft.TextField(hint_text="seu nome")
+    message = ft.TextField(hint_text="sua mensagem...", expand=True)
+    send = ft.ElevatedButton(text="Enviar", on_click=send_click)
+    page.add(messages, ft.Row([user, message, send]))
 
 
 if __name__ == "__main__":
-    ft.app(main)
+    ft.app(main, view=ft.AppView.WEB_BROWSER,port=8080)
